6927171ce001d5c8b278cc57828a14fd164352dd
Ameer Mouky <ameer.mouky@yahoo.com>
2019-11-18 17:13:47 -0800

    12 Hours in Updates

--- js/script.js
+++ js/script.js
@@ -1,50 +1,155 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+let studentsList = document.querySelectorAll('.student-item');
+const studentsListContainer = document.querySelector('.student-list');
+const pages = document.querySelector('.page');
+const studentsListPages = 10;
+const beginningPageNum = 1;
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+//showpage function displays the list of student per page once the link is clicked.
+const showPage = (list, page) => {
+// firstIndex and lastIndex variables retreive the index for the first and last student on the on a particular page.
+  const firstIndex = page * studentsListPages - studentsListPages
+  const lastIndex = page * studentsListPages
 
-
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+//loop through the list of student and show or hide students based on the page number that was clicked.
+  for(let i = 0; i < list.length; i++) {
+      if(i >= firstIndex && i < lastIndex){
+        list[i].style.display = '';
+      } else {
+        list[i].style.display = 'none';
+      }
+  }
+}
 
 
 
+const appendPageLinks = (list) => {
+  const numOfPage = Math.ceil(list.length / studentsListPages);
+  const div = document.createElement('div');
+  const ul = document.createElement('ul');
+  div.className = 'pagination';
+  div.appendChild(ul);
+  pages.appendChild(div);
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+// create links for each page
+  for(i = 1; i <= numOfPage; i++) {
+    const li = document.createElement('li');
+    const a = document.createElement('a')
+// add page number
+    a.innerHTML = `${i}`;
+    a.href = '#';
+    li.appendChild(a);
+    ul.appendChild(li);
+// highlight current page 
+    if(i == beginningPageNum){
+      a.className = 'active';
+    }
+  }
 
 
 
+  const links = document.querySelectorAll('a');
+// loop through 
+  for(i = 0; i < links.length; i++) {
+    const link = links[i];
+    link.addEventListener('click', (e) => {
+// loop through each link, make sure only one link has a class active.
+      for(i = 0; i < links.length; i++) {
+        if (links[i].innerHTML == e.target.innerHTML) {
+          links[i].className = 'active'
+        } else {
+          links[i].className = ''
+        }
+      }
+      showPage(list, e.target.innerHTML);
+    });
+  }
+}
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+// call both appendPageLinks and showPage function to load links
+showPage(studentsList, beginningPageNum);
+appendPageLinks(studentsList);
+let paginationContainer = document.querySelector('.pagination')
+
+
+// select and create input tag
+const pageHeader = document.querySelector('.page-header');
+const searchDiv = document.createElement('div');
+searchDiv.className = 'student-search';
+const input = document.createElement('input');
+input.placeholder="Search for students...";
+input.id = 'search-input';
+const button = document.createElement('button');
+button.innerHTML = 'Search';
+button.id = 'search';
+searchDiv.appendChild(input);
+searchDiv.appendChild(button);
+pageHeader.appendChild(searchDiv);
+
+//select input with id '#search-input', searchButton tag with id '#search' and select the names of all student from the h3 tag.
+const searchInput = document.querySelector('#search-input');
+const searchButton = document.querySelector('#search');
+const students = document.querySelectorAll('.student-item .student-details h3');
+
+//create a no results message and append it to the DOM
+let noMatch;
+const p  = document.createElement('p')
+const text = 'No results, Please search for another name.';
+p.innerText = text;
+p.id = 'no-results'
+pages.insertBefore(p, studentsListContainer)
+noMatch = studentsListContainer.previousElementSibling;
+noMatch.style.display = 'none';
+
+//noResult function displays the "no results" message when the input is empty or when there is not a match.
+const noResult = () => {
+  studentsListContainer.style.display = 'none';
+  noMatch.style.display = '';
+}
+
+
+//searchFunction loops through list of names to only display letters or names that matches the input value.
+const searchFunction = (searchInput, students) => {
+  const names = [];
+  const input = searchInput.value.toLowerCase().trim();
+  paginationContainer = document.querySelector('.pagination');
+
+  if(paginationContainer !== null){
+    pages.removeChild(paginationContainer);
+  }
+
+  if(input.length === 0){
+    noMatch.style.display = 'none';
+    studentsListContainer.style.display = ''
+    showPage(studentsList, beginningPageNum);
+    appendPageLinks(studentsList);
+  } else  {
+    for(let i = 0; i < students.length; i++) {
+      students[i].style.display = 'none';
+      const studentNode = students[i].childNodes[1].childNodes[3].textContent;
+      if(input.length !== 0 && studentNode.includes(input)){
+        names.push(students[i])
+      }
+    }
+
+    if(names.length === 0){
+      noResult();
+    } else {
+      studentsListContainer.style.display = '';
+      noMatch.style.display = 'none';
+      showPage(names, 1);
+      appendPageLinks(names);
+    }
+  }
+}
+
+searchInput.addEventListener('keyup', () => {
+  searchFunction(searchInput, studentsList);
+});
+
+searchButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  searchFunction(searchInput, studentsList);
+  searchInput.value = '';
+});
